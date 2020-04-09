d83446f64d69e14e11b6338cb8629b4491a949cd
Alexandr <alex@alex>
2020-04-09 05:54:21 +0300

    intermediate commit.

--- src/main/java/repository/OrderBookRepository.java
+++ src/main/java/repository/OrderBookRepository.java
@@ -3,21 +3,21 @@ package repository;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import repository.model.Quote;
 
 import javax.persistence.Query;
 import java.util.List;
 
-@Repository
 public class OrderBookRepository {
 
     private Log logger = LogFactory.getLog(this.getClass());
 
-    @Autowired
     private SessionFactory sessionFactory;
+
+    public void setSessionFactory(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
 
     @Transactional
     public void fillTable(List<Quote> quotes) {}
